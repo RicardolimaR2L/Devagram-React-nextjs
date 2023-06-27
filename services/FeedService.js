738d989be9ccbe9a6e1089a026b5c5d011e79a5d
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-06-27 19:25:56 -0300

    correcao do retorno do metodo put

--- services/FeedService.js
+++ services/FeedService.js
@@ -5,8 +5,15 @@ export default class FeedService extends HttpService {
     let url = '/feed'
     if (idUsuario) {
       //verifica se temos usuário e passa o id interpolando com a url do feed
-      url +=`id=${idUsuario}` //url dinâmica
+      url +=`?id=${idUsuario}` //url dinâmica
     }
     return this.get(url)
   }
+
+  async adicionarComentario(idPostagem, comentario){
+     return this.put(`/comentario?id=${idPostagem}`,{
+      comentario
+     });
+  }
+
 }
