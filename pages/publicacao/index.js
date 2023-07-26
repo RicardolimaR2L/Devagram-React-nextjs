0c1c830742943e60492c6b28544b8c0f0f826220
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-07-26 12:53:00 -0300

    estrutura e estilos da pagina de publicacao

---
+++ pages/publicacao/index.js
@@ -0,0 +1,41 @@
+import CabecalhoComAcoes from '@/componentes/cabecalhoComAcoes'//
+import { UploadImagem } from '@/componentes/uploadImagem'//
+import comAutorizacao from '@/hoc/comAutorizacao'//
+import { useState } from 'react'//
+import imgPublicacao from '@/public/imagens/imgPublicacao.svg'//
+import Botao from '@/componentes/botao'//
+
+function publicacao() {
+  const [imagem, setImagem] = useState()
+  const [inputImagem, setInputImagem] = useState()
+
+  return (
+    <div className="paginaPublicacao largura30pctDesktop">
+          <h1>Nova publicação</h1>
+          <CabecalhoComAcoes
+            textoEsquerda={'Cancelar'}
+            elementoDireita={'Avançar'}
+          />
+      <div className="conteudoPaginaPublicaçao">
+        <div className="primeiraEtapa">
+          <UploadImagem
+            setImagem={setImagem}
+            aoSetarAReferencia={setInputImagem}
+            imagemPreviewClassName={imagem ? 'previewImagemPublicacao' : ''}
+            imagemPreview={imagem?.preview || imgPublicacao.src}
+          />
+          <span className="desktop textoDragAndDrop">
+            Arraste sua foto aqui.{' '}
+          </span>
+
+          <Botao
+            texto={'Selecionar uma imagem'}
+            manipularClique={() => console.log('teste ok ')}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default comAutorizacao(publicacao)
